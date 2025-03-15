42a94f03a9ad8c11cdf13ba3d6c12af0310bce5b
chekh <chechirkinbogd@gmail.com>
2025-03-15 15:32:39 +0200

    add filter django DJANGO SUCCESS

--- django/transactions/urls.py
+++ django/transactions/urls.py
@@ -4,4 +4,5 @@ from .views import TransactionViewSet
 urlpatterns = [
     path("transactions/", TransactionViewSet.as_view({"get": "list", "post": "create"}), name="transactions-list"),
     path("transactions/<int:pk>/", TransactionViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}), name="transactions-detail"),
+    path("transactions/summary/", TransactionViewSet.as_view({"get": "summary"}), name="transactions-summary"),
 ]
--- django/transactions/views.py
+++ django/transactions/views.py
@@ -2,13 +2,63 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from .models import Transaction
 from .serializers import TransactionSerializer
+from django.utils.dateparse import parse_date
+from django.db.models import Sum
+from rest_framework.decorators import action
+
 
 class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    @action(detail=False, methods=["get"], url_path="summary")
+    def summary(self, request):
+        user = request.user
+
+        total_income = Transaction.objects.filter(user=user, transaction_type="income").aggregate(Sum("amount"))["amount__sum"] or 0
+        total_expense = Transaction.objects.filter(user=user, transaction_type="expense").aggregate(Sum("amount"))["amount__sum"] or 0
+        balance = user.balance
+        return Response({
+            "total_income": total_income,
+            "total_expense": total_expense,
+            "balance": balance
+        })
+
     def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user)
+        queryset = Transaction.objects.filter(user=self.request.user)
+        params = self.request.query_params
+
+        date = params.get("date")
+        if date:
+            parsed_date = parse_date(date)
+            if parsed_date:
+                queryset = queryset.filter(created_at__date=parsed_date)
+
+        category = params.get("category")
+        if category:
+            queryset = queryset.filter(category=category)
+
+        transaction_type = params.get("type")
+        if transaction_type in ["income", "expense"]:
+            queryset = queryset.filter(transaction_type=transaction_type)
+
+        return queryset
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        transaction = serializer.save(user=self.request.user)
+        if transaction.transaction_type == "income":
+            self.request.user.balance += transaction.amount
+        else:
+            self.request.user.balance -= transaction.amount
+        self.request.user.save()
+
+    def destroy(self, request, *args, **kwargs):
+        transaction = self.get_object()
+
+        if transaction.transaction_type == "income":
+            request.user.balance -= transaction.amount
+        else:
+            request.user.balance += transaction.amount
+
+        request.user.save()
+        return super().destroy(request, *args, **kwargs)
